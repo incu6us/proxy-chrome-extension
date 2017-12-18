94c84c46e478a7aa760d8591be144a27f4739197
Vyacheslav Pryimak <vyacheslavpryimak@gmail.com>
2017-12-18 15:14:14 +0200

    setting proxy via url parameter refactored

--- js/background.js
+++ js/background.js
@@ -1,122 +1,133 @@
 var Proxy = function () {
 
-    var storedData = {};
+        var storedData = {};
 
-    var config = function (proxyAddress) {
-        if (proxyAddress === undefined || proxyAddress === "") {
+        var config = function (proxyAddress) {
+            if (proxyAddress === undefined || proxyAddress === "") {
+                return {
+                    mode: "direct"
+                };
+            }
             return {
-                mode: "direct"
-            };
-        }
-        return {
-            mode: "fixed_servers",
-            rules: {
-                proxyForHttp: {
-                    host: proxyAddress.split(":")[0],
-                    port: parseInt(proxyAddress.split(":")[1])
-                },
-                proxyForHttps: {
-                    host: proxyAddress.split(":")[0],
-                    port: parseInt(proxyAddress.split(":")[1])
-                },
-                bypassList: ["localhost,127.0.0.1"]
+                mode: "fixed_servers",
+                rules: {
+                    proxyForHttp: {
+                        host: proxyAddress.split(":")[0],
+                        port: parseInt(proxyAddress.split(":")[1])
+                    },
+                    proxyForHttps: {
+                        host: proxyAddress.split(":")[0],
+                        port: parseInt(proxyAddress.split(":")[1])
+                    },
+                    bypassList: ["localhost,127.0.0.1"]
+                }
             }
-        }
-    };
+        };
 
-    var credentialsToHeader = function (details, proxyUsername, proxyPassword) {
-        if ((proxyUsername === undefined && proxyPassword === undefined) || (proxyUsername === "" && proxyPassword === "")) {
+        var credentialsToHeader = function (details, proxyUsername, proxyPassword) {
+            if ((proxyUsername === undefined && proxyPassword === undefined) || (proxyUsername === "" && proxyPassword === "")) {
+                return {requestHeaders: details.requestHeaders};
+            }
+
+            for (var header in details.requestHeaders) {
+                if (header.name == 'Authorization') {
+                    return {};
+                }
+            }
+
+            details.requestHeaders.push({
+                name: 'Authorization',
+                value: 'Basic ' + btoa(proxyUsername + ':' + proxyPassword)
+            });
+
             return {requestHeaders: details.requestHeaders};
-        }
+        };
 
-        for (var header in details.requestHeaders) {
-            if (header.name == 'Authorization') {
-                return {};
+        var authCredentials = function (proxyUsername, proxyPassword) {
+            return {
+                authCredentials: {
+                    username: proxyUsername,
+                    password: proxyPassword
+                }
             }
-        }
+        };
 
-        details.requestHeaders.push({
-            name: 'Authorization',
-            value: 'Basic ' + btoa(proxyUsername + ':' + proxyPassword)
-        });
-
-        return {requestHeaders: details.requestHeaders};
-    };
-
-    var authCredentials = function (proxyUsername, proxyPassword) {
-        return {
-            authCredentials: {
-                username: proxyUsername,
-                password: proxyPassword
-            }
-        }
-    };
-
-    Proxy.prototype.setProxy = function (proxyAddress, proxyUsername, proxyPassword) {
-        if (proxyAddress === undefined || proxyAddress.trim() === "") {
-            chrome.proxy.settings.set({value: config(), scope: 'regular'});
-        } else {
-            chrome.proxy.settings.set(
-                {value: config(proxyAddress), scope: 'regular'},
-                function () {
-                    if (proxyAddress !== "" && proxyUsername !== "") {
-                        if (chrome.webRequest.onAuthRequired) {
-                            chrome.webRequest.onAuthRequired.addListener(function (details) {
-                                return authCredentials(proxyUsername.trim(), proxyPassword.trim());
-                            }, {urls: ['<all_urls>']}, ['blocking']);
-                        } else {
-                            chrome.webRequest.onBeforeSendHeaders.addListener(function (details) {
-                                return credentialsToHeader(details, proxyUsername.trim(), proxyPassword.trim());
-                            }, {urls: ['<all_urls>']}, ['blocking', 'requestHeaders']);
+        Proxy.prototype.setProxy = function (proxyAddress, proxyUsername, proxyPassword) {
+            if (proxyAddress === undefined || proxyAddress.trim() === "") {
+                chrome.proxy.settings.set({value: config(), scope: 'regular'});
+            } else {
+                chrome.proxy.settings.set(
+                    {value: config(proxyAddress), scope: 'regular'},
+                    function () {
+                        if (proxyAddress !== "" && proxyUsername !== "") {
+                            if (chrome.webRequest.onAuthRequired) {
+                                chrome.webRequest.onAuthRequired.addListener(function (details) {
+                                    return authCredentials(proxyUsername.trim(), proxyPassword.trim());
+                                }, {urls: ['<all_urls>']}, ['blocking']);
+                            } else {
+                                chrome.webRequest.onBeforeSendHeaders.addListener(function (details) {
+                                    return credentialsToHeader(details, proxyUsername.trim(), proxyPassword.trim());
+                                }, {urls: ['<all_urls>']}, ['blocking', 'requestHeaders']);
+                            }
                         }
                     }
-                }
-            );
-        }
-        debugProxySettings();
-    };
-
-    var debugProxySettings = function () {
-        chrome.proxy.settings.get(
-            {'incognito': false},
-            function (config) {
-                console.log("Proxy settings: " + JSON.stringify(config));
-                console.log("Auth settings: " + JSON.stringify(
-                    {
-                        username: storedData.proxyUsername,
-                        password: storedData.proxyPassword
-                    }
-                ));
+                );
             }
-        );
-    };
+            debugProxySettings(proxyUsername, proxyPassword);
+        };
 
-    var init = function () {
-        if (Object.keys(storedData).length !== 0) {
-            Proxy.prototype.setProxy(storedData.proxyAddress, storedData.proxyUsername, storedData.proxyPassword);
-        }
-        chrome.storage.onChanged.addListener(function (changes, namespace) {
-            // for (k in changes)
+        var debugProxySettings = function (proxyUsername, proxyPassword) {
+                chrome.proxy.settings.get(
+                    {'incognito': false},
+                    function (config) {
+                        console.log("Proxy settings: " + JSON.stringify(config));
+                        if (proxyUsername !== undefined) {
+                            console.log("Auth settings: " + JSON.stringify(
+                                {
+                                    username: proxyUsername,
+                                    password: proxyPassword
+                                }
+                            ));
+                        } else {
+                            console.log("Auth settings: " + JSON.stringify(
+                                {
+                                    username: storedData.proxyUsername,
+                                    password: storedData.proxyPassword
+                                }
+                            ));
+                        }
+                    }
+                );
+            }
+        ;
+
+        var init = function () {
+            if (Object.keys(storedData).length !== 0) {
+                Proxy.prototype.setProxy(storedData.proxyAddress, storedData.proxyUsername, storedData.proxyPassword);
+            }
+            chrome.storage.onChanged.addListener(function (changes, namespace) {
+                // for (k in changes)
+                chrome.storage.sync.get(
+                    null,
+                    function (items) {
+                        storedData = items
+                        Proxy.prototype.setProxy(storedData.proxyAddress, storedData.proxyUsername, storedData.proxyPassword);
+                    }
+                );
+            });
+        };
+
+        this.run = function () {
             chrome.storage.sync.get(
                 null,
                 function (items) {
                     storedData = items
-                    Proxy.prototype.setProxy(storedData.proxyAddress, storedData.proxyUsername, storedData.proxyPassword);
+                    init();
                 }
             );
-        });
-    };
-
-    this.run = function () {
-        chrome.storage.sync.get(
-            null,
-            function (items) {
-                storedData = items
-                init();
-            }
-        );
-    };
-};
+        };
+    }
+;
 
 
 var ProxyByURL = function () {
@@ -161,32 +172,28 @@ var ProxyByURL = function () {
      * "blpProxyAddress", "blpProxyUsername", "blpProxyPassword" - to pass proxy parameters
      */
     this.run = function () {
-        var finalUrl = undefined;
+        var url = undefined;
+        var tabId = undefined;
         var blpProxyParams = ["blpProxyAddress", "blpProxyUsername", "blpProxyPassword"];
 
         chrome.webRequest.onBeforeRequest.addListener(function (details) {
-            chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-                if (tabs.length > 0 && tabs[0].hasOwnProperty("url")) {
-                    var url = tabs[0].url;
-                    console.log("Loading URL: " + url)
-                    var urlParams = parseQueryString(url);
-                    if (urlParams.blpProxyAddress !== undefined) {
-                        // console.log("Navigation: " + JSON.stringify(details));
-                        // console.log("params: " + JSON.stringify(urlParams));
-                        ProxyByURL.prototype.setProxy(urlParams.blpProxyAddress, urlParams.blpProxyUsername, urlParams.blpProxyPassword);
-                        finalUrl = removeBlpUrlParams(blpProxyParams, url);
-                    }
+            if (details.frameId === 0 && details.type === "main_frame") {
+                url = details.url;
+                tabId = details.tabId;
+                var urlParams = parseQueryString(url);
+                if (urlParams !== undefined && urlParams.blpProxyAddress !== undefined) {
+                    ProxyByURL.prototype.setProxy(urlParams.blpProxyAddress, urlParams.blpProxyUsername, urlParams.blpProxyPassword);
+                    console.log("URL: " + url);
+                    return {redirectUrl: removeBlpUrlParams(blpProxyParams, url)};
                 }
-            });
-            if (finalUrl !== undefined) {
-                return {redirectUrl: finalUrl};
-            }
-        }, {urls: ['<all_urls>'], types: ['main_frame']}, ['blocking']);
 
-        chrome.tabs.onUpdated.addListener(function (tabId, info) {
-            if (info.status === "complete") {
+            }
+        }, {urls: ['<all_urls>']}, ['blocking']);
+
+        chrome.tabs.onUpdated.addListener(function (tab, info) {
+            if (tab === tabId && info.status === "complete") {
                 ProxyByURL.prototype.setProxy();
-                console.log("Completed loading URL: " + finalUrl);
+                console.log("Completed loading URL: " + url);
             }
         });
     };
